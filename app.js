638ecb7e52d3b9d3b9295eb1947360d0b5117252
Sara Gramstad <sara.gramstad@gmail.com>
2026-04-16 12:40:30 +0200

    Refactor: Move shared shell into EJS public layout

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 
+var expressLayouts = require('express-ejs-layouts');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
@@ -14,6 +16,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/public');
 
 app.use(logger('dev'));
 app.use(express.json());
